9eabf0730d76023b6dbc3b9bc8639884367862d5
Anupam Pal <144058044+Hunkey-Pilot@users.noreply.github.com>
2024-04-24 23:11:51 +0600

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//advance optimized;
+void adv_bubble_sort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+                {break;}
+    }
+}
+
+//normal sort;
+void bubble_sort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+void display(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    //bubble_sort(a, n);
+    adv_bubble_sort(a, n);
+    display(a, n);
+    return 0;
+}
